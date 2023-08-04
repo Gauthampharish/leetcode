71b7fe6e5ad79069bd1126e2ca1e31007a6c318e
Gautham P Harish <90624687+Gauthampharish@users.noreply.github.com>
2023-08-04 10:43:29 +0530

    Update Contains Duplicate.py
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
    
     
    
    Example 1:
    
    Input: s = "anagram", t = "nagaram"
    Output: true
    Example 2:
    
    Input: s = "rat", t = "car"
    Output: false
     
    
    Constraints:
    
    1 <= s.length, t.length <= 5 * 104
    s and t consist of lowercase English letters.

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -7,3 +7,16 @@ class Solution(object):
             n.add(nums[i])
         return False
             
+or 
+
+
+
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        if(len(s)==len(t)):
+            res = Counter(s)
+            res1=Counter(t)
+            if(res==res1):
+                return True
+        return False
